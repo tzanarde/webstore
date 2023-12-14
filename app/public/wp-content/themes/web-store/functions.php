5581ec2923d431d66d7e5cd0db52da9f86574f86
Tiago Zanarde <zanarde.tiago@gmail.com>
2023-12-14 15:24:36 -0300

    woocommerce install and themes removed

--- app/public/wp-content/themes/web-store/functions.php
+++ app/public/wp-content/themes/web-store/functions.php
@@ -21,6 +21,30 @@
             array('web_store_main_menu' => 'Web Store Main Menu',
                   'web_store_footer_menu' => 'Web Store Footer Menu')
         );
+
+        add_theme_support(
+            'woocommerce',
+            array(
+                'thumbnail_image_width' => 255,
+                'single_image_width' => 255,
+                'product_grid' => array(
+                    'default_rows' => 10,
+                    'min_rows' => 5,
+                    'max_rows' => 10,
+                    'default_columns' => 1,
+                    'min_columns' => 1,
+                    'max_columns' => 4
+                )
+            )
+        );
+
+        add_theme_support('wc-product-gallery-zoom');
+        add_theme_support('wc-product-gallery-lightbox');
+        add_theme_support('wc-product-gallery-slider');
+
+        if (!isset($content_width)) {
+            $content_width = 600;
+        }
     }
 
-    add_action('after_setup_theme', 'web_store_config');
+    add_action('after_setup_theme', 'web_store_config', 0);
